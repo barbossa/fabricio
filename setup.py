3f3dd038ba9203c922f917704f9409cdfdd3a8f6
Vladimir Kalyuzhny <vladimir@kalyuzhny.pro>
2020-07-25 20:32:08 +0300

    Bump version to 0.5.9

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name='fabricio',
-    version='0.5.8',
+    version='0.5.9',
     author='Rinat Khabibiev',
     author_email='srenskiy@gmail.com',
     packages=list(map('fabricio.'.__add__, find_packages('fabricio'))) + ['fabricio'],
